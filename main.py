571d7166eab724d0a7ac510b17f765b4b3d5751f
Shahriyar <116467943+Shahriyar-1988@users.noreply.github.com>
2025-07-16 16:38:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,33 +4,33 @@ from src.pipeline.data_transformation_pipeline import DataTransformationPipeline
 from src.pipeline.training_pipeline import TrainingPipeline
 from src.pipeline.evaluation_pipeline import EvaluationPipeline
 
-# logger.info("Welcome to etherium transactions fraud detection project")
-# STAGE_NAME = "Data Ingestion Stage"
-# try:
-#     logger.info(f">>>>{STAGE_NAME} started <<<<")
-#     obj=DataIngestionPipeline()
-#     obj.initiate_data_ingestion()
-#     logger.info(f">>>> {STAGE_NAME} completed <<<<\n\n")
-# except Exception as e:
-#     raise e
+logger.info("Welcome to etherium transactions fraud detection project")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>{STAGE_NAME} started <<<<")
+    obj=DataIngestionPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>> {STAGE_NAME} completed <<<<\n\n")
+except Exception as e:
+    raise e
 
-# STAGE_NAME = "Data Transformation Stage"
-# try:
-#     logger.info(f">>>>{STAGE_NAME} started <<<<")
-#     obj=DataTransformationPipeline()
-#     obj.initiate_data_transformation()
-#     logger.info(f">>>> {STAGE_NAME} completed <<<<\n\n")
-# except Exception as e:
-#     raise e
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>{STAGE_NAME} started <<<<")
+    obj=DataTransformationPipeline()
+    obj.initiate_data_transformation()
+    logger.info(f">>>> {STAGE_NAME} completed <<<<\n\n")
+except Exception as e:
+    raise e
 
-# STAGE_NAME = "Model training Stage"
-# try:
-#     logger.info(f">>>>{STAGE_NAME} started <<<<")
-#     obj=TrainingPipeline()
-#     obj.initiate_model_training()
-#     logger.info(f">>>> {STAGE_NAME} completed <<<<\n\n")
-# except Exception as e:
-#     raise e
+STAGE_NAME = "Model training Stage"
+try:
+    logger.info(f">>>>{STAGE_NAME} started <<<<")
+    obj=TrainingPipeline()
+    obj.initiate_model_training()
+    logger.info(f">>>> {STAGE_NAME} completed <<<<\n\n")
+except Exception as e:
+    raise e
 
 STAGE_NAME = "Model evaluation Stage"
 try:
